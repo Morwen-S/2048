6a18222395d3d2c37e0b2c39311cd68aea7c68fd
Svetlana Butina <svetabbutina@outlook.com>
2021-01-22 14:13:55 +0600

    added start components

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,75 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="header">
+      <h1 class="title">2048</h1>
+      <div class="control">
+        <div class="score">1125</div>
+        <div class="record">2048</div>
+      </div>
+    </div>
+    <game-field></game-field>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheGameField from '@/components/TheGameField.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    'game-field': TheGameField
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+* {
+  margin: 0;
+  padding: 0;
+  font-size: 18px;
+  font-family: 'Oswald', sans-serif;;
 }
+
+body {
+  background-color: #fffcf7;
+  color: #898989;
+}
+.container {
+  width: 500px;
+  margin: 0 auto;
+  margin-top: 30px;
+}
+
+.header {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 30px;
+}
+
+.title {
+  display: block;
+  font-size: 70px;
+  font-weight: bold;
+}
+
+.control {
+  display: flex;
+  justify-content: space-around;
+  margin-top: 10px;
+}
+
+.control > div {
+  display: block;
+  color: white;
+  font-size: 20px;
+  font-weight: bold;
+  height: 25px;
+  margin: 10px 0 10px 10px;
+  padding: 10px 25px;
+  border-radius: 5px;
+  background-color: #898989;
+}
+
 </style>
---
+++ src/components/TheGameField.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="game-container">
+    <div class="grid-container">
+      <div class="grid-row" v-for="y in 4" :key="y">
+        <div class="grid-cell" v-for="x in 4" :key="y + x"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    function createField (size) {
+      let field = []
+      for (let y = 0; y < size; y++) {
+        field.push([])
+        for (let x = 1; x < size; x++) {
+          field[y].push(null)
+        }
+      }
+      return field
+    }
+    const field = ref(createField())
+
+    return {
+      field
+    }
+  }
+}
+</script>
+
+<style>
+.game-container {
+  width: 500px;
+  height: 500px;
+  background-color: #898989;
+  border-radius: 10px;
+  margin: 0 auto;
+}
+
+.grid-container {
+  display: block;
+  width: 100%;
+  height: 100%;
+  padding: 10px;
+}
+
+.grid-container :last-child {
+  margin-bottom: 0;
+}
+
+.grid-row {
+  display: block;
+  height: calc((100% - (10px * 5)) / 4);
+  margin-bottom: 10px;
+}
+
+.grid-row :last-child {
+  margin-right: 0;
+}
+
+.grid-cell {
+  display: inline-block;
+  border-radius: 5px;
+  height: 100%;
+  width: calc((100% - (10px * 5)) / 4);
+  margin-right: 10px;
+  background-color: #767676;
+}
+</style>
